6948ffaee027cd3921218149dcb4eceac211edcc
WeizhongTu <tuweizhong@163.com>
2020-04-16 19:02:49 +0800

    add array params encode test

--- encode_test.go
+++ encode_test.go
@@ -5,12 +5,22 @@ import (
 )
 
 var (
-	tests = []struct {
+	mapParamsTests = []struct {
 		Params Params
 		Err    string
 	}{
 		{
 			Params: Params{"": ""},
+			Err:    "error expected: xml: start tag with no name",
+		},
+	}
+
+	arrayParamsTests = []struct {
+		Params ArrayParams
+		Err    string
+	}{
+		{
+			Params: ArrayParams{{"", ""}},
 			Err:    "error expected: xml: start tag with no name",
 		},
 	}
@@ -22,7 +32,21 @@ func TestClient_MarshalXML(t *testing.T) {
 		t.Errorf("error not expected: %s", err)
 	}
 
-	for _, test := range tests {
+	for _, test := range mapParamsTests {
+		_, err = soap.Call("checkVat", test.Params)
+		if err == nil {
+			t.Errorf(test.Err)
+		}
+	}
+}
+
+func TestClient_MarshalXML2(t *testing.T) {
+	soap, err := SoapClient("http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl")
+	if err != nil {
+		t.Errorf("error not expected: %s", err)
+	}
+
+	for _, test := range arrayParamsTests {
 		_, err = soap.Call("checkVat", test.Params)
 		if err == nil {
 			t.Errorf(test.Err)
