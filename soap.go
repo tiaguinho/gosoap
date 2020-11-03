05f75150f206fe4d6a2a5b70ff8792f3c5b98726
Konstantin <32811996+requiemofthesouls@users.noreply.github.com>
2020-11-03 10:23:42 +0500

    verbose error message when status code is not ok

--- soap.go
+++ soap.go
@@ -237,7 +237,13 @@ func (p *process) doRequest(url string) ([]byte, error) {
 	}
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
-		return nil, errors.New("unexpected status code: " + resp.Status)
+		responseBodyBytes, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, fmt.Errorf("Unexpected status code: %d.\nDetail: %s", resp.StatusCode, responseBodyBytes)
 	}
 
 	return ioutil.ReadAll(resp.Body)
