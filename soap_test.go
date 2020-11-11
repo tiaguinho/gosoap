b50259259ffc21bed32d61459692e2d48681ee61
keep_on_rail <dimamid99@gmail.com>
2020-11-11 09:29:35 +0500

    fix doRequest test with status code fail

--- soap_test.go
+++ soap_test.go
@@ -256,7 +256,7 @@ func TestProcess_doRequest(t *testing.T) {
 		t.Errorf("err can't be nil")
 	}
 
-	if err != nil && err.Error() != "unexpected status code: 404 Not Found" {
+	if err != nil && err.Error() != "Unexpected status code: 404" {
 		t.Errorf("unexpected error: %s", err)
 	}
 }
